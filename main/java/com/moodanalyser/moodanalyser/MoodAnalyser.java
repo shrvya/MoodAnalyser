b81f641b9a5c715ea706b41b633dded38f824380
shrvya <shrivyashetty@gmail.com>
2021-09-08 20:47:02 +0530

    added:welcome to mood analyser

---
+++ main/java/com/moodanalyser/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,11 @@
+package com.moodanalyser.moodanalyser;
+
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to mood analyser");
+
+	}
+
+}
---
+++ test/java/com/moodanalyser/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,10 @@
+package com.moodanalyser.moodanalyser;
+
+public class MoodAnalyserTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
